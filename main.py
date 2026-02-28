063b84888b3857bbb4a6e19d15392c53aa2d5388
Guilherme de Oliveira <102908318+Guilherm12122@users.noreply.github.com>
2026-02-27 22:59:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from lexers.lexer import tokenize
 from parsers.parser import Parser
 
-code_test_1 = "1+2;1+4"
+# code_test_1 = "1+2;1+4"
+# code_test_2 = "1+3*5"
+code_test_3 = "1+2*3"
 
-tokens = tokenize(code_test_1)
+# tokens = tokenize(code_test_1)
+tokens = tokenize(code_test_3)
 
 # print(tokens)
 
 parser = Parser(tokens)
 ast = parser.parse_program()
 
-# print(ast)
+print(ast)
+print(ast[0].right)
+print(ast[0].left)

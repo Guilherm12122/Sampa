077443c7fbbc0e7aac8d757fd7465aef830d4c76
Guilherme de Oliveira <102908318+Guilherm12122@users.noreply.github.com>
2026-03-27 21:22:01 -0300

    remove list within list

--- lexers/lexer.py
+++ lexers/lexer.py
@@ -5,29 +5,36 @@ DATA_TYPES = [
     'BOOLEANO'
 ]
 
-KEY_WORDS = [
-    'INTEIRO'
-]
+KEY_WORDS = ['SE', 'SENAO'] + DATA_TYPES
+
+def list_into_string(lst):
+
+    return ''.join(lst)
+
+
+def get_token_len(token):
+
+    return len(token[1])
+
 
 def check_data_type(stmt, j):
 
     for dt in DATA_TYPES:
 
         lenght_dt = len(dt)
+        print(stmt[j:j+lenght_dt])
 
-        if stmt[j:j+lenght_dt] == dt:
+        if list_into_string(stmt[j:j+lenght_dt]) == dt:
 
             return dt
-        
-
-# def check_data_type(stmt, j):
-
-#     return list(filter(lambda dt : stmt[j:j + len(dt)] == dt, DATA_TYPES))[0]
 
 
 def check_boolean(stmt, j):
 
-    return stmt[j:j+5] == "FALSO" or stmt[j:j+10] == "VERDADEIRO"
+    check_false = list_into_string(stmt[j:j+5]) == "FALSO"
+    check_true = list_into_string(stmt[j:j+10]) == "VERDADEIRO"
+
+    return check_false or check_true
     
 
 def get_identifier(stmt, j):
@@ -45,172 +52,137 @@ def get_identifier(stmt, j):
 
     return identifier, j
 
+def get_identifier_or_datatype(stmt, j):
 
-
-def check_identifier(stmt, j):
-
-    identifier = ''
+    check_nat = ''
 
     while j < len(stmt) and (stmt[j].isalnum() or stmt[j] == '_'):
 
-        identifier += stmt[j]
+        check_nat += stmt[j]
         j += 1
 
-    if identifier not in KEY_WORDS:
+    if check_nat and check_nat not in KEY_WORDS:
 
-        return identifier
+        return ('IDENTIFIER', check_nat)
+    
+    elif check_nat in DATA_TYPES:
+
+        return ('DATA_TYPE', check_nat)
 
 
 
 def tokenize(code_str: str):
     
-    list_stmt = code_str.split(';')
+    stmt = list(code_str)
 
-    i = 0
+    j = 0
     tokens = []
 
-    while i < len(list_stmt):
+    while j < len(stmt):
 
-        stmt = list_stmt[i]
-        tuples_stmt = []
-        j = 0
+        c = stmt[j]
 
-        while j < len(stmt):
+        if c.isspace():
+            j += 1
 
-            c = stmt[j]
+        elif c.isdigit():
+            num = ""
+            has_dot = False
 
-            if check_identifier(stmt, j):
-
-                identifier = check_identifier(stmt, j)
-
-                tuples_stmt.append(('IDENTIFIER', identifier))
-
-                j += len(identifier)
-
-            elif c.isspace():
+            while j < len(stmt) and (stmt[j].isdigit() or stmt[j] == '.'):
+                if stmt[j] == '.':
+                    if has_dot:
+                        raise Exception("Float malformado")
+                    has_dot = True
+                num += stmt[j]
                 j += 1
 
-            elif c.isdigit():
-                num = ""
-                has_dot = False
-
-                while j < len(stmt) and (stmt[j].isdigit() or stmt[j] == '.'):
-                    if stmt[j] == '.':
-                        if has_dot:
-                            raise Exception("Float malformado")
-                        has_dot = True
-                    num += stmt[j]
-                    j += 1
-
-                if has_dot:
-                    tuples_stmt.append(("FLUTUANTE", num))
-                else:
-                    tuples_stmt.append(("INTEIRO", num))
-
-            elif c == "+":
-                tuples_stmt.append(("PLUS", "+"))
-                j += 1
-
-            elif c == '-':
-                tuples_stmt.append(("MINUS", "-"))
-                j += 1
-
-            elif c == '/':
-                tuples_stmt.append(("DIVISION", "/"))
-                j += 1
-
-            elif c == '%':
-                tuples_stmt.append(("MODULUS", "%"))
-                j += 1
-
-            elif c == '=':
-                tuples_stmt.append(("ASSIGN", "="))
-                j += 1
-
-            elif c == "(":
-                tuples_stmt.append(("OpenParen", "("))
-                j += 1
-
-            elif c == ")":
-                tuples_stmt.append(("CloseParam", ")"))
-                j += 1
-
-            elif c == "*":
-                tuples_stmt.append(("MULTIPLICATION", "*"))
-                j += 1
-
-            elif stmt[j:j+2] == '==':
-                tuples_stmt.append(("EQUALS", "=="))
-                j += 2
-
-            elif stmt[j:j+2] == '!=':
-                tuples_stmt.append(("NOT_EQUALS", "!="))
-                j += 2
-
-            elif stmt[j:j+7] == "MOSTRAR":
-                tuples_stmt.append(("PRINT", "MOSTRAR"))
-                j += 7
-
-            elif c in ["'", '"']:
-
-                j += 1
-
-                string = ''
-
-                while j < len(stmt) and stmt[j] != c:
-
-                    string += stmt[j]
-
-                    j += 1
-
-                tuples_stmt.append(('STRING', string))
-
-                j += 1
-
-            elif check_boolean(stmt, j):
-
-                if stmt[j:j+10] == "VERDADEIRO":
-                    boolean = "VERDADEIRO"
-                else:
-                    boolean = "FALSO"
-
-                tuples_stmt.append(('BOOLEAN', boolean))
-
-                j += len(boolean)
-
-            elif check_data_type(stmt, j):
-
-                data_type = check_data_type(stmt, j)
-
-                tuples_stmt.append(('DATA_TYPE', data_type))
-
-                j += len(data_type)
-
-                # if dt:
-                    
-                #     tuples_stmt.append(dt)
-
-                #     j += leng
-
-                #     identifier, j = get_identifier(stmt, j)
-
-                #     tuples_stmt.append(('IDENTIFIER', identifier.strip()))
-
-            # O que nao for válido anterior, é um identifier (paser vai validar semantica -> tem data type antes ?? tem assing (=) depois??)
-            # elif c.isalpha() or c == '_':
-            #     identifier = ''
-
-            #     while j < len(stmt) and (stmt[j].isalnum() or stmt[j] == '_'):
-            #         identifier += stmt[j]
-            #         j += 1
-
-            #     tuples_stmt.append(('IDENTIFIER', identifier))
-
+            if has_dot:
+                tokens.append(("FLUTUANTE", num))
             else:
+                tokens.append(("INTEIRO", num))
+
+        elif c == "+":
+            tokens.append(("PLUS", "+"))
+            j += 1
+
+        elif c == '-':
+            tokens.append(("MINUS", "-"))
+            j += 1
+
+        elif c == '/':
+            tokens.append(("DIVISION", "/"))
+            j += 1
+
+        elif c == '%':
+            tokens.append(("MODULUS", "%"))
+            j += 1
+
+        elif c == '=':
+            tokens.append(("ASSIGN", "="))
+            j += 1
+
+        elif c == "(":
+            tokens.append(("OpenParen", "("))
+            j += 1
+
+        elif c == ")":
+            tokens.append(("CloseParam", ")"))
+            j += 1
+
+        elif c == "*":
+            tokens.append(("MULTIPLICATION", "*"))
+            j += 1
+
+        elif list_into_string(stmt[j:j+2]) == '==':
+            tokens.append(("EQUALS", "=="))
+            j += 2
+
+        elif list_into_string(stmt[j:j+2]) == '!=':
+            tokens.append(("NOT_EQUALS", "!="))
+            j += 2
+
+        elif list_into_string(stmt[j:j+7]) == "MOSTRAR":
+            tokens.append(("PRINT", "MOSTRAR"))
+            j += 7
+
+        elif c in ["'", '"']:
+
+            j += 1
+
+            string = ''
+
+            while j < len(stmt) and stmt[j] != c:
+
+                string += stmt[j]
+
+                j += 1
+
+            tokens.append(('STRING', string))
+
+            j += 1
+
+        elif check_boolean(stmt, j):
+
+            if list_into_string(stmt[j:j+10]) == "VERDADEIRO":
+                boolean = "VERDADEIRO"
+            else:
+                boolean = "FALSO"
+
+            tokens.append(('BOOLEAN', boolean))
+
+            j += len(boolean)
+
+        else:
+            
+            undef = get_identifier_or_datatype(stmt, j)
+
+            if not undef:
+
                 raise Exception(f"Caractere inválido: {c}")
             
-        tokens.append(tuples_stmt)
-        
-        i += 1
+            tokens.append(undef)
+
+            j += get_token_len(undef)
 
     return tokens
